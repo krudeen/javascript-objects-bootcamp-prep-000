2fa320fdafa78022a85a5a7cd344b3c79c58e379
Kyle Rudeen <krudeen@gmail.com>
2018-07-28 19:20:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,14 +1,3 @@
-var playlist = { artistName: "songTitles" };
-
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName] = 'songTitle'; 
-  return playlist;
-}
-
-function removeFromPlaylist(playlist, artistName){
-  delete playlist.artistName;
-  return playlist;
-}
 
 var playlist = {artistName: songTitle};
 function updatePlaylist (playlist, artistName, songTitle) {
